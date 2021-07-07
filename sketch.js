5dc1532c86c72ecb1a7e8164535e3ff642a01ce6
vivaan28 <76045934+vivaan28@users.noreply.github.com>
2021-07-07 23:36:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+block1 = new Block (310,275,20,40);
+block2 = new Block (330,275,20,40);
+block3 = new Block (360,275,20,40);
+block4 = new Block (390,275,20,40);
+block5 = new Block (420,275,20,40);
+block6 = new Block (450,275,20,40);
+block7 = new Block (480,275,20,40);
+//level two
+block8 = new Block (330,235,30,40);
+block9 = new Block(360,235,30,40);
+block10 = new Block(390,235,30,40);
+block11 = new Block(420,235,30,40);
+block12 = new Block(450,235,30,40);
+//level three
+block13 = new Block(360,195,30,40);
+block14 = new Block(390, 195,30,40);
+block15 = new Block(420,195,30,40);
+//top
+block16 = new Block(390,155,30,40);
+
